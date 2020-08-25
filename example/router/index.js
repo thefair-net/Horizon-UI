9d040708ef407188d78d16e1343e272b60b8112b
李秋奇 <mail@qiuqi.li>
2020-08-25 11:45:52 +0800

    优化：demo路由跳转时，scroll top

--- example/router/index.js
+++ example/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+
 const NavConfig = require('./nav.config.json');
 
 Vue.use(VueRouter)
@@ -22,9 +23,22 @@ const routeRegister = config => {
   return routes
 }
 
+const scrollBehavior = (to, from, savedPosition) => {
+  if (savedPosition) {//返回之前的原位置
+    // savedPosition 当且仅当 popstate 导航 (通过浏览器的 前进/后退 按钮触发) 时才可用。
+    return savedPosition
+  } else {
+    const position = {}
+    position.x = 0
+    position.y = 0
+    return position
+  }
+}
+
 const router = new VueRouter({
   mode: 'hash',
   base: process.env.BASE_URL,
+  scrollBehavior,
   routes: routeRegister(NavConfig)
 })
 
