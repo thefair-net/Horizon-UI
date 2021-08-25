f8db16c3dff3ab1d57714ba47d9a28bfa00fbd3f
李秋奇@work <mail@qiuqi.li>
2021-08-25 15:09:51 +0800

    更新：新增 checkbox

---
+++ example/pages/basic/checkbox.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="page">
+    <div class="cell">
+      checkbox: <vs-checkbox :type="'checkbox'" v-model="value1"/><span @click="() => {value1 = !value1}">描述选项 [size regular]</span>
+    </div>
+    <div class="cell">
+      checkbox: <vs-checkbox :type="'checkbox'" v-model="value1" :size="'large'" :color="'red'"/><span @click="() => {value1 = !value1}">描述选项 [size large]</span>
+    </div>
+    <div class="cell">
+      radio: <vs-checkbox :type="'radio'" v-model="value2"/><span @click="() => {value2 = !value2}">描述选项 [size regular]</span>
+    </div>
+    <div class="cell">
+      radio: <vs-checkbox :type="'radio'" v-model="value2" :size="'mini'" :color="'green'"/><span @click="() => {value2 = !value2}">描述选项 [size mini]</span>
+    </div>
+    <div class="cell">
+      disabled: <vs-checkbox :type="'checkbox'" v-model="value3" disabled/><span @click="() => {value3 = !value3}">描述选项 [disabled]</span>
+    </div>
+  </div>
+</template>
+
+<script>
+import {Checkbox} from "../../../lib";
+
+export default {
+  name: "radio",
+  components: {
+    'vs-checkbox': Checkbox
+  },
+  data() {
+    return {
+      value1: true,
+      value2: true,
+      value3: false
+    }
+  }
+}
+</script>
+
+<style>
+._checkbox {
+  margin-left: 15px;
+}
+</style>
+
+<style lang="scss" scoped>
+.page {
+}
+.cell {
+  padding: 15px;
+  width: 100%;
+  height: 56px;
+  border: 1px #999999 solid;
+  display: flex;
+  align-items: center;
+
+  span {
+    margin-left: 5px;
+  }
+}
+
+</style>
---
+++ src/components/checkbox.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="_checkbox">
+    <template v-if="type === 'checkbox'">
+      <div class="_Checkbox" :class="{_large: size === 'large', _mini: size === 'mini'}" :style="{borderColor: color, opacity: disabled && 0.2}">
+        <div v-if="value" class="_checked" :style="{backgroundColor: color}"/>
+      </div>
+    </template>
+    <template v-if="type === 'radio'">
+      <div class="_Checkbox _Radio" :class="{_large: size === 'large', _mini: size === 'mini'}" :style="{borderColor: color, opacity: disabled && 0.2}">
+        <div v-if="value" class="_checked _Radio" :style="{backgroundColor: color}"/>
+      </div>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "checkbox",
+  props: {
+    type: {
+      default: 'checkbox', // radio checkbox
+      required: true,
+      type: String
+    },
+    size: {
+      default: 'regular', // mini large 10px...
+      required: false,
+      type: String
+    },
+    value: {
+      default: false,
+      required: true,
+      type: Boolean
+    },
+    color: {
+      default: 'black',
+      required: false,
+      type: String
+    },
+    disabled: {
+      default: false,
+      type: Boolean
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+._checkbox {
+  ._Checkbox {
+    width: 12px;
+    height: 12px;
+    border: 1px black solid;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    padding: 1px;
+  }
+
+  ._Radio {
+    border-radius: 50%;
+    overflow: hidden;
+  }
+
+  ._checked {
+    width: 100%;
+    height: 100%;
+    background-color: black;
+  }
+
+  ._large {
+    width: 15px;
+    height: 15px;
+  }
+
+  ._mini {
+    width: 10px;
+    height: 10px;
+  }
+}
+</style>
